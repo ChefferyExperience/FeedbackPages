7e61571b258a342f1939df13a946a78d9969543a
Aaron <aaron.james2@ontariotechu.net>
2026-03-03 20:29:10 -0500

    Should work now

--- web/flutter_service_worker.js
+++ web/flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "5520f8fd2c9a11d4314ba99290cf60de",
+const RESOURCES = {"flutter_bootstrap.js": "5c50fe1be3ea9633ecf5fd880bb0ad2e",
 "version.json": "b66fdceb52927ae2915a7ab97980aa8d",
 "index.html": "ad0cfe4a960cdbd024f585e35b928bb3",
 "/": "ad0cfe4a960cdbd024f585e35b928bb3",
-"main.dart.js": "66a9f3d8f8d8e5a8e74deabedc30e33a",
+"main.dart.js": "381e4e17adb93031952d0bb436da9c05",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
