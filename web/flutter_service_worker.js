60b32c93dfe9d02aae94988a410548c3f3d573cc
Aaron <aaron.james2@ontariotechu.net>
2026-03-18 17:51:59 -0400

    removed verbose

--- web/flutter_service_worker.js
+++ web/flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "2c406178fcfa493c5c411835aa6bc67b",
+const RESOURCES = {"flutter_bootstrap.js": "2345a09df08e7d5a8994892c99b1cc75",
 "version.json": "b66fdceb52927ae2915a7ab97980aa8d",
 "index.html": "2d479bd6d26ad901789cd8a755e59bec",
 "/": "2d479bd6d26ad901789cd8a755e59bec",
-"main.dart.js": "aa4bc0e53d4c1985705d876153d294a3",
+"main.dart.js": "52774f2e56152affe2402711a6b03598",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
